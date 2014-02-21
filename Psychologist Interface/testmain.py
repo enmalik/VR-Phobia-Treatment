8cb9eb9c2f53f2fda388f4b7532418d49c18ee0d
Nahiyan Malik <contact@nahiyan.com>
2014-02-21 06:26:54 -0500

    Implemented creating and loading users.

--- Psychologist Interface/testmain.py
+++ Psychologist Interface/testmain.py
@@ -4,21 +4,18 @@ sys.path.append("/Users/nahiyanmalik/Development/wxPython-src-3.0.0.0/wxPython")
 import wx
 import pigui
 import os
+import json
 
-import serial
-import time
-import datetime
-import re
 
-from threading import *
-
-resetTime = wx.DateTimeFromDMY(31, wx.DateTime.Dec, 2000)
+resetDate = wx.DateTimeFromDMY(31, wx.DateTime.Dec, 2000)
 
 appDirectory = "/Users/nahiyanmalik/Development/VR-Phobia-Treatment/Psychologist Interface/App/"
 patientDirectory = appDirectory + "Patients/"
 simDirectory = appDirectory + "Simulations/"
 
-patientList = ["Select Existing Patient"]
+patientPath = []
+
+patientList = []
  
 class MainApp(pigui.PsychologistInterfaceFrame):
     # def OnInit(self):
@@ -39,34 +36,55 @@ class MainApp(pigui.PsychologistInterfaceFrame):
         self.patientPanel.historyListPanel = PanelHistory(self.patientPanel.historyNotebookPanel)
         self.patientPanel.historyInfoPanel = PanelHistoryInfo(self.patientPanel.historyNotebookPanel)
 
+        self.setPanelTypes()
         self.loadUsers()
 
+        # ONCE IS FINALIZED, SEND IN PANEL AND USE .type IN DEF
         self.updatePanel("intro")
         self.updateHistoryPanel("list");
 
         self.introPanel.newCreatePatientBtn.Bind( wx.EVT_BUTTON, self.changeIntroPanel )
-        self.introPanel.patientChoice.Bind( wx.EVT_CHOICE, self.selectUser )
+        self.introPanel.patientChoice.Bind( wx.EVT_CHOICE, self.selectPatient )
 
         self.createPanel.cancelPatientBtn.Bind( wx.EVT_BUTTON, self.cancelCreate )
         self.createPanel.createPatientBtn.Bind( wx.EVT_BUTTON, self.createPatient )
 
     # def firstPanel(self, parent):
 
+    def setPanelTypes(self):
+        self.introPanel.type = "intro"
+        self.createPanel.type = "create"
+        self.patientPanel.type = "patient"
+        self.patientPanel.historyListPanel.type = "list"
+        self.patientPanel.historyInfoPanel.type = "info"
+
     def loadUsers(self):
-        patientList.extend(os.listdir(patientDirectory))
+        self.introPanel.patientChoice.Clear()
+        global patientList
+        patientList = ["Select Existing Patient"]
+
+        for item in os.listdir(patientDirectory):
+            if not item.startswith('.'):
+                patientList.append(item)
+
+        # patientList.extend(os.listdir(patientDirectory))
+
         self.introPanel.patientChoice.AppendItems(patientList)
         print "loaded users"
         print "patient list: ", patientList
 
 
-    def selectUser(self, event):
+    def selectPatient(self, event):
         selectedUser = self.introPanel.patientChoice.GetStringSelection()
         selectedIndex = self.introPanel.patientChoice.GetCurrentSelection()
 
         print selectedUser
+        print self.introPanel.type
         
         if selectedIndex != 0:
-            self.updatePanel("patient")
+            global patientPath
+            patientPath = patientDirectory + selectedUser + "/"
+            self.goToUser("patient", selectedUser)
 
         self.resetIntroPanel()
 
@@ -76,25 +94,85 @@ class MainApp(pigui.PsychologistInterfaceFrame):
     def resetCreatePanel(self):
         self.createPanel.fnameTextCtrl.Clear()
         self.createPanel.lnameTextCtrl.Clear()
-        self.createPanel.dobDatePicker.SetValue(resetTime)
+        self.createPanel.dobDatePicker.SetValue(resetDate)
         self.createPanel.genderChoice.SetSelection(0)
         self.createPanel.patientIdTextCtrl.Clear()
         self.createPanel.notesTextCtrl.Clear()
+
+    def goToUser(self, panel, user):
+        self.updatePanel(panel)
+
+        patientJson = patientPath + "info.json"
+
+        if os.path.exists(patientJson):
+            with open(patientJson, 'r') as f:
+                userData = json.load(f)
+
+            date = wx.DateTimeFromDMY(userData['dobDay'], userData['dobMonth'], userData['dobYear'])
+            
+            self.patientPanel.fnameTextCtrl.SetValue(userData['firstName'])
+            self.patientPanel.lnameTextCtrl.SetValue(userData['lastName'])
+            self.patientPanel.dobDatePicker.SetValue(date)
+            self.patientPanel.genderChoice.SetSelection(userData['gender'])
+            self.patientPanel.patientIdTextCtrl.SetValue(userData['patientID'])
+            self.patientPanel.notesTextCtrl.SetValue(userData['notes'])
+
+        else:
+            wx.MessageBox('Could not load user', 'Problem', wx.OK | wx.ICON_INFORMATION)
+
+
 
 
 
     def changeIntroPanel( self, event ):
         self.updatePanel("create")
 
-    def createPatient(self, event):
-        self.updatePanel("patient")
+    # def createPatient(self, event):
+    #     self.updatePanel("patient")
 
     def cancelCreate(self, event):
-        print type(self.createPanel.dobDatePicker.GetValue())
-        self.resetCreatePanel()
         self.updatePanel("intro")
+        self.resetCreatePanel()
 
-        # CREATE METHOD TO CLEAR ALL FIELDS
+    def createPatient(self, event):
+        if self.createPanel.fnameTextCtrl.GetValue() == "" or self.createPanel.lnameTextCtrl.GetValue() == "" or self.createPanel.patientIdTextCtrl.GetValue() == "":
+            wx.MessageBox('Please fill out all the mandory (*) fields', 'Missing Information', wx.OK | wx.ICON_INFORMATION)
+        else:
+            newUser = self.createPanel.fnameTextCtrl.GetValue() + " " + self.createPanel.lnameTextCtrl.GetValue()
+            global patientPath
+            patientPath = patientDirectory + newUser + "/"
+            if os.path.exists(patientPath):
+                wx.MessageBox('Please enter a different name', 'Existing User', wx.OK | wx.ICON_INFORMATION)
+            else:
+                os.makedirs(patientPath)
+
+                infoJsonPath = patientPath + "info.json"
+
+                dob = self.createPanel.dobDatePicker.GetValue()
+                dobDay, dobMonth, dobYear = dob.Day, dob.Month, dob.Year
+
+                print dobDay, dobMonth, dobYear
+
+                userData = {
+                    "firstName":self.createPanel.fnameTextCtrl.GetValue(),
+                    "lastName":self.createPanel.lnameTextCtrl.GetValue(),
+                    "dobDay":dobDay,
+                    "dobMonth":dobMonth,
+                    "dobYear":dobYear,
+                    "gender":self.createPanel.genderChoice.GetSelection(),
+                    "patientID":self.createPanel.patientIdTextCtrl.GetValue(),
+                    "notes":self.createPanel.notesTextCtrl.GetValue()
+                }
+
+                with open(infoJsonPath, 'w') as f:
+                    json.dump(userData, f)
+
+                print userData
+
+                self.loadUsers()
+                self.goToUser("patient", newUser)
+                self.resetCreatePanel()
+        
 
     def updatePanel(self, panel):
         if panel == "intro":
